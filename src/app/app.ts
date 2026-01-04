ab937e2a052a34bd5033d7c1ac397096aacc5bea
Mario Ahumada <ahumadamob@gmail.com>
2026-01-04 16:32:35 -0300

    feat: extract navbar header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Leftbar } from './layout/leftbar/leftbar';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Leftbar],
+  imports: [RouterOutlet, Leftbar, Header],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
